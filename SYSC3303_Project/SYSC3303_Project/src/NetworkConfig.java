306225eeec483594e979f9ae8e462bbbf7d4a06f
Sai Janjanam <sai@Sais-MacBook-Pro.local>
2018-07-09 17:21:12 -0400

    Added server functionaility, along with RRQ and WRQ connection verifications and responses, and shutting ddown gracefully

---
+++ SYSC3303_Project/SYSC3303_Project/src/NetworkConfig.java
@@ -0,0 +1,10 @@
+
+public class NetworkConfig {
+	/**
+	 * This class contains the network configuration settings
+	 */
+	
+	// should be changed to 69 when submitting the project
+	public static final int SERVER_PORT = 6000;
+	public static final int DATAGRAM_PACKET_MAX_LEN = 100;
+}
